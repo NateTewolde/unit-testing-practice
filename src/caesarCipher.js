a587a3f10d6ca3387cb2d9e9cf98880e1292d8b4
NateTewolde <tewolde.nate@gmail.com>
2022-09-15 01:47:50 -0700

    Update shiftChar() to correctly shift all charcters by the key amount

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -8,13 +8,44 @@ function caesarCipher(string, key) {
 }
 
 function shiftChar(strChar, key) {
-  let shiftedUnicode;
-  if (strChar.charCodeAt(0) === 32) {
-    shiftedUnicode = 32;
+  let shiftedUnicode = 0;
+  if (
+    strChar.charCodeAt(0) + key > 122 &&
+    strChar.charCodeAt(0) >= 97 &&
+    strChar.charCodeAt(0) <= 122
+  ) {
+    let difference = strChar.charCodeAt(0) + key - 122;
+    shiftedUnicode += difference + 97;
+  } else if (
+    strChar.charCodeAt(0) + key < 97 &&
+    strChar.charCodeAt(0) >= 97 &&
+    strChar.charCodeAt(0) <= 122
+  ) {
+    let difference = strChar.charCodeAt(0) + key - 96;
+    shiftedUnicode += difference + 122;
+  } else if (
+    strChar.charCodeAt(0) + key > 90 &&
+    strChar.charCodeAt(0) >= 65 &&
+    strChar.charCodeAt(0) <= 90
+  ) {
+    let difference = strChar.charCodeAt(0) + key - 90;
+    shiftedUnicode += difference + 65;
+  } else if (
+    strChar.charCodeAt(0) + key < 65 &&
+    strChar.charCodeAt(0) >= 65 &&
+    strChar.charCodeAt(0) <= 90
+  ) {
+    let difference = strChar.charCodeAt(0) + key - 64;
+    shiftedUnicode += difference + 90;
+  } else if (strChar.toLowerCase() != strChar.toUpperCase()) {
+    shiftedUnicode += strChar.charCodeAt(0) + key;
+  } else {
+    shiftedUnicode += strChar.charCodeAt(0);
   }
-  if (strChar.charCodeAt(0) + key > 122) {
-  }
+
   return String.fromCharCode(shiftedUnicode);
 }
+
+shiftChar("A", -1);
 
 export default caesarCipher;
